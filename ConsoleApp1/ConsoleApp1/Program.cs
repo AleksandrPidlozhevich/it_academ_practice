2149d287a6f02cf55850f42f8aa973dbac4b5e70
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-02-22 20:51:34 +0300

    3task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,24 +15,46 @@ namespace ConsoleApp1
         }
         static void OneDimensionalArray()
         {
-            object[] array2 = new object[3] { 32, 'A', "Hello" };
+            object[] arr2 = new object[3] { 32, 'A', "Hello" };
 
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(array2[i]);
+                Console.WriteLine(arr2[i]);
             }
 
-            int intBuffer = Convert.ToInt32(array2[0]);
+            int intBuffer = Convert.ToInt32(arr2[0]);
             intBuffer += 10;
-            array2[0] = intBuffer;
-            string stringBuffer = Convert.ToString(array2[2]);
+            arr2[0] = intBuffer;
+            string stringBuffer = Convert.ToString(arr2[2]);
             stringBuffer += " guys!";
-            array2[2] = stringBuffer;
+            arr2[2] = stringBuffer;
 
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(array2[i]);
+                Console.WriteLine(arr2[i]);
             }
+        }
+        static void Max13Array()
+        {
+            Random rand = new Random();
+            int arr3buffer = 0;
+            int[] arr3 = new int[13];
+            for (int i = 0; i < arr3.Length - 1; i++)
+            {
+                arr3[i] = rand.Next(0, 50);
+            }
+            for (int i = 0; i < arr3.Length - 1; i++)
+            {
+                if (arr3[i] > arr3buffer)
+                {
+                    arr3buffer = arr3[i];
+                }
+            }
+            for (int i = 0; i < arr3.Length - 1; i++)
+            {
+                Console.WriteLine(arr3[i]);
+            }
+            Console.WriteLine($"Макс. знач: {arr3buffer}");
         }
     }
 }
