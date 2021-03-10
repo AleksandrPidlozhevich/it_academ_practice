ce9cd38137ecdac3cbcfe75a02f48ac7bdadd2e2
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-03-10 20:12:41 +0300

    haven't decided yet

---
+++ Lecture_10/Lecture_10/Engine.cs
@@ -0,0 +1,23 @@
+﻿using Lecture_10;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_12
+{
+    class Engine : Motorcycle
+    {
+        public int PowerMotorcycle(int side)
+        {
+            Random rnd = new Random();
+            int power = rnd.Next(50, 30);
+            return power;
+        }
+        public double VolumeMotorcycle(int radius)
+        {
+            Random rnd = new Random();
+            int volume = rnd.Next(125, 3200);
+            return volume;
+        }
+    }
+}
---
+++ Lecture_10/Lecture_10/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lecture_10
+{
+    class Motorcycle
+    {
+        public string Model { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public int Year { get; set; }
+
+        public int Identifier { get; set; }
+        public int Ident()
+        {
+            var guid = Guid.NewGuid().ToString();
+        }
+
+        public string ToString()
+        {
+            return $" {Manufacturer} {Model} {Year} ";
+        }
+    }
+}
