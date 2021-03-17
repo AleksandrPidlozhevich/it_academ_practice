1f352bbfe9cf9bc3a6c2074964fd8b775b9bc192
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-03-17 20:49:01 +0300

    Update Figure.cs

--- lecture_12/lecture_12/Figure.cs
+++ lecture_12/lecture_12/Figure.cs
@@ -21,14 +21,13 @@ namespace lecture_12
     public class circle : Square
     {
         public string name;
-        const int pi = 3;
         public int r;
 
         public circle()
         {
             name = "circle";
             Random rnd = new Random();
-            r = rnd.Next(1, 10);
+            r = rnd.Next(2, 10);
         }
 
         public double Square()
@@ -45,23 +44,24 @@ namespace lecture_12
     public class triangle : Square
     {
         public string name;
-        public int len;
-        public int hight;
+        public int a;
+        public int b;
+        public int c;
+        public int p;
 
         public triangle()
         {
             name = "triangle";
-            Random rnd = new Random();
-            int len = rnd.Next(1, 10);
-            int hight = rnd.Next(1, 10);
         }
-
-
         public double Square()
         {
-            return (double)(len * hight * 0.5);
+            Random rnd = new Random();
+            int a = rnd.Next(2, 10);
+            int b = rnd.Next(2, 10);
+            int c = rnd.Next(2, 10);
+            double pol = (a + b + c) / 2;
+            return (double)(Math.PI * pol);
         }
-
         int Square.Square()
         {
             throw new NotImplementedException();
